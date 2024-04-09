5aa0a88325d526b6a046228e9fc3cb9ca9f07ca8
海屿有燕 <zorua@vip.qq.com>
2024-04-10 06:04:57 +0800

    备份匠心文件

---
+++ add-modules.bat
@@ -0,0 +1,164 @@
+git submodule add https://github.com/wikimedia/mediawiki-extensions-AJAXPoll AJAXPoll
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-AbuseFilter.git AbuseFilter
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-AdvancedSearch AdvancedSearch
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://github.com/TopRealm/mediawiki-extensions-AntiSpoof-fork AntiSpoof
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.wiki/YSARXIV/MW-Avatar.git Avatar
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-BetaFeatures BetaFeatures
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CategoryTree CategoryTree
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CheckUser CheckUser
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Cite Cite
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CiteThisPage CiteThisPage
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Citoid Citoid
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CodeMirror CodeMirror
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/Fork/mediawiki-extensions-CollapsibleSidebar CollapsibleSidebar
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Fork/mediawiki-extensions-CommonsMetadata CommonsMetadata
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-ConfirmEdit ConfirmEdit
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.wiki/YSARXIV/mediawiki-extensions-ContributionScores ContributionScores
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CirrusSearch CirrusSearch
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CreateUserPage CreateUserPage
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/Fork/mediawiki-extensions-DarkMode DarkMode
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Disambiguator Disambiguator
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-DiscussionTools DiscussionTools
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Echo Echo
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Elastica Elastica
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-EmbedVideo EmbedVideo
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-EventLogging EventLogging
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-FileImporter FileImporter
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Fork/mediawiki-extensions-Gadgets Gadgets
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-ImageMap ImageMap
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-InputBox InputBox
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Interwiki Interwiki
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-InterwikiExtracts InterwikiExtracts
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-LabeledSectionTransclusion LabeledSectionTransclusion
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Linter Linter
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Lockdown Lockdown
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-LoginNotify LoginNotify
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-MassEditRegex MassEditRegex
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-MassMessage MassMessage
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/Fork/mediawiki-extensions-Matomo Matomo
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-MediaSearch MediaSearch
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-MultiBoilerplate MultiBoilerplate
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-MultimediaViewer MultimediaViewer
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-NativeSvgHandler NativeSvgHandler
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-NewSignupPage NewSignupPage
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Nuke Nuke
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-OATHAuth OATHAuth
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-OAuth OAuth
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-OrphanedTalkPages OrphanedTalkPages
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-PageForms PageForms
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-PageImages PageImages
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-ParserFunctions ParserFunctions
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-PdfHandler PdfHandler
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-PinyinSort PinyinSort
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Poem Poem
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Popups Popups
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Renameuser Renameuser
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-ReplaceText ReplaceText
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-SecureLinkFixer SecureLinkFixer
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-SyntaxHighlight_GeSHi SyntaxHighlight_GeSHi
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TemplateData TemplateData
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TemplateSandbox TemplateSandbox
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TemplateStyles TemplateStyles
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TemplateWizard TemplateWizard
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TextExtracts TextExtracts
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Thanks Thanks
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TwoColConflict TwoColConflict
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-UploadWizard UploadWizard
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-UserMerge UserMerge
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-WikiEditor WikiEditor
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-WikiLove WikiLove
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-WikiSEO WikiSEO
+timeout /t 3 /nobreak > NUL
+git submodule add -b master https://git.qiuwen.wiki/Fork/mediawiki-extensions-cldr cldr
+timeout /t 3 /nobreak > NUL
+git submodule add https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-PWA PWA
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-NewUserMessage NewUserMessage
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-SpamBlacklist SpamBlacklist
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-TitleBlacklist TitleBlacklist
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-CodeEditor CodeEditor
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-SandboxLink SandboxLink
+timeout /t 3 /nobreak > NUL
+git submodule add https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-TemplateStylesExtender TemplateStylesExtender
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-RevisionSlider RevisionSlider
+timeout /t 3 /nobreak > NUL
+git submodule add -b REL1_39 https://git.qiuwen.net.cn/Mirror/mediawiki-extensions-Scribunto Scribunto
+timeout /t 3 /nobreak > NUL
+git submodule add https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-RegexFunctions RegexFunctions
+timeout /t 3 /nobreak > NUL
+git submodule add https://git.qiuwen.net.cn/YSARXIV/mediawiki-extensions-Widgets Widgets
+timeout /t 3 /nobreak > NUL
---
+++ generate.py
@@ -0,0 +1,52 @@
+import os
+import time
+
+# 检测当前目录是否存在.gitmodules文件
+if os.path.exists('.gitmodules'):
+    print("发现.gitmodules文件")
+else:
+    print("未找到.gitmodules文件")
+    exit()
+
+# 读取.gitmodules文件内容
+with open('.gitmodules', 'r') as file:
+    content = file.read()
+
+# 分割每个子模块的信息
+modules = content.split('[submodule')
+
+# 打开add-modules.bat文件用于写入命令
+with open('add-modules.bat', 'w') as file:
+    # 遍历每个子模块
+    for module in modules[1:]:  # 跳过第一个空元素
+        print(f"正在处理模块: {module.strip()}")
+        
+        # 提取模块的路径、URL和分支信息
+        path = None
+        url = None
+        branch = None
+        for line in module.split('\n'):
+            line = line.strip()  # 去除行首尾的空格
+            if line.lower().startswith('path'):
+                path = line.split('=')[1].strip()
+            elif line.lower().startswith('url'):
+                url = line.split('=')[1].strip()
+            elif line.lower().startswith('branch'):
+                branch = line.split('=')[1].strip()
+        
+        # 生成子模块添加命令
+        command = 'git submodule add'
+        if branch:
+            command += f' -b {branch}'
+        command += f' {url} {path}'
+        
+        print(f"生成的命令: {command}")
+        
+        # 将命令写入add-modules.bat文件
+        file.write(command + '\n')
+        file.write('timeout /t 3 /nobreak > NUL\n')  # 添加3秒延迟
+        
+        print("模块命令生成成功")
+        time.sleep(3)  # 间隔3秒防止控制台拥堵
+
+print("任务完成")
