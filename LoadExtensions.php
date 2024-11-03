052aaba5a790ec1e0a98d72cabf70d24ec62bdb3
海屿有燕 <zorua@vip.qq.com>
2024-11-03 18:28:14 +0800

    feat: 加载 UserProfileV2 扩展

--- LoadExtensions.php
+++ LoadExtensions.php
@@ -319,3 +319,5 @@ $wgTmhEnableMp4Uploads = true;
 
 // If you use ffmpeg 2, it can be set to true
 $wgUseFFmpeg2 = false;
+
+wfLoadExtension( 'UserProfileV2', "/www/wwwroot/mw-utils/YsArchives-Extensions/UserProfileV2/extension.json" );
