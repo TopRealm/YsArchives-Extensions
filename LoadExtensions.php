b0802eb5d4da8e36a199751373b0c2f12c021239
海屿有燕 <zorua@vip.qq.com>
2025-03-10 17:09:45 +0800

    fix: 更新SemanticResultFormats扩展的查询结果缓存类型

--- LoadExtensions.php
+++ LoadExtensions.php
@@ -265,7 +265,7 @@ wfLoadExtension( 'SemanticResultFormats', "/www/wwwroot/mw-utils/YsArchives-Exte
 # 语义维基调优
 $smwgQMaxLimit = 5000;
 $smwgQEqualitySupport = SMW_EQ_NONE;
-$smwgQueryResultCacheType = CACHE_ACCEL;
+$smwgQueryResultCacheType = CACHE_ANYTHING;;
 $smwgQMaxSize = 10;
 $wgDefaultUserOptions['smw-prefs-general-options-show-entity-issue-panel'] = false;
 $smwgMaintenanceLanguage = 'zh';
