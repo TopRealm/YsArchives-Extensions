15b287e730084544d9ef4ac1154938e063a28cf5
海屿有燕 <zorua@vip.qq.com>
2025-04-12 03:14:52 +0800

    fix: 更新Math扩展的默认用户选项和有效模式为'mathjax'

--- LoadExtensions.php
+++ LoadExtensions.php
@@ -368,5 +368,5 @@ $wgTmhEnableMp4Uploads = true;
 $wgUseFFmpeg2 = false;
 
 wfLoadExtension( 'Math', "$ysyExtensionsPath/Math/extension.json" );
-$wgDefaultUserOptions['math'] = 'native';
-$wgMathValidModes = ['native'];
+$wgDefaultUserOptions['math'] = 'mathjax';
+$wgMathValidModes = ['mathjax'];
